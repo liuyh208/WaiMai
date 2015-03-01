090bd5f37c00526921b510fe2be44daf5fbfa37c
liuyh208 <liuyh208@sohu.com>
2015-03-01 22:26:42 +0800

    init

---
+++ WaiMai.DAL/RepositoryFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaiMai.IDAL;
+
+namespace WaiMai.DAL
+{
+    public static class RepositoryFactory
+    {
+        //public static IBaseUserRepository UserInfoRepository
+        //{
+        //    get { return new BaseUserRepository(); }
+        //}
+
+        //public static IBaseRoleRepository RoleRepository
+        //{
+        //    get { return new BaseRoleRepository(); }
+        //}
+
+       
+    }
+}
---
+++ WaiMai.UI/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using WaiMai.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WaiMai.UI.Portal.Controllers
+{
+    public class HomeController :BaseController
+    {
+        public ActionResult Index()
+        {
+            BaseUser user = Session["UserInfo"] as BaseUser;
+            if (user != null)
+            {
+                ViewBag.UserName = user.RealName;
+            }
+
+            return View();
+        }
+
+    }
+}
