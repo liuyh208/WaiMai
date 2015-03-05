03bc2b561b08a0c5cba9631770a7cc315b568af7
liuyh208 <liuyh208@sohu.com>
2015-03-05 22:51:13 +0800

    增加logo

---
+++ WaiMai.UI/Controllers/OrderManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WaiMai.UI.Portal.Controllers
+{
+    public class OrderManagerController : Controller
+    {
+        //
+        // GET: /OrderManager/
+
+        public ActionResult Start()
+        {
+            return View();
+        }
+
+    }
+}
